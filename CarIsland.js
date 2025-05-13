978f83e6571c332e4b9435230f317880d47fd4e7
JustN1tro <camdenritchie95@gmail.com>
2025-05-13 11:10:20 -0400

    Update CarIsland.js

--- CarIsland.js
+++ CarIsland.js
@@ -7,11 +7,18 @@ var carDialog = "";
 var clickNum = 0;
 var hasInteractedWithCar = false;
 const car = document.getElementById('carImage');
+const carButton = document.getElementById('carImageButton');
 const carDialogBox = document.querySelector('.carDialogText');
+// Car Emotions
+const carEmotion = {
+  "neutral" : "https://static.wikia.nocookie.net/garn47/images/f/f8/Car_fluff.png/revision/latest/scale-to-width/360?cb=20240810200531",
+  "mad" : "https://static.wikia.nocookie.net/garn47/images/5/5a/Onlyimage.png/revision/latest/scale-to-width/360?cb=20240731011139"
+}
 // Car Dialog Typewriter Effect
 var typeWriterDone = true;
 var i = 0;
 var speed = 50; // The speed/duration of the effect in milliseconds
+
 
 // Carousel Logic
 function carousel()
@@ -34,42 +41,47 @@ function typeWriter(txt)
   if (i == txt.length)
   {
     typeWriterDone = true;
+    if (clickNum == 3)
+    {
+      window.close();
+    }
   }
 }
 
-car.addEventListener("click", () => {
+carButton.addEventListener("click", () => {
   if (hasInteractedWithCar == false && typeWriterDone == true)
   {
     clickNum++;
     typeWriterDone = false;
     carDialog = "Hello! I am Car!";
     hasInteractedWithCar = true;
-    updateCarText();
+    updateCar();
   }
   else if (clickNum == 1 && typeWriterDone == true)
   {
     clickNum++;
     typeWriterDone = false;
     carDialog = "Why are you still clicking on me?";
-    updateCarText();
+    updateCar();
   }
   else if (clickNum == 2 && typeWriterDone == true)
   {
     clickNum++;
     typeWriterDone = false;
     carDialog = "Stop it! Or I'll do something you won't like!";
-    updateCarText();
+    car.src = carEmotion["mad"];
+    updateCar();
   }
   else if (clickNum == 3 && typeWriterDone == true)
   {
     clickNum++;
     typeWriterDone = false;
     carDialog = "Fine! You asked for it!";
-    updateCarText();
+    updateCar();
   }
 });
 
-function updateCarText() {
+function updateCar() {
   if (carDialog == "")
   {
     carDialogBox.style.visibility = "hidden";
